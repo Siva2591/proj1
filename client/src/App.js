8c7008729e5d356cef16325852b015d3c23ddab7
Siva2591 <sunkarivenkatasiva638@gmail.com>
2024-03-12 12:26:04 +0530

    a

--- client/src/App.js
+++ client/src/App.js
@@ -56,77 +56,69 @@
 // export default App;
 
 
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage';
 import Cards from './Pages/Home';
 import Process from './Pages/Process';
 import Settings from './Pages/Settings';
 import Protected from './components/protected';
-
-// Initialize Google Analytics
-const TRACKING_ID = 'G-T0TQC4PPCN'; // Replace with your own tracking ID
+import RouteChangeTracker from './RouteChangeTracker';
 
 function App() {
-  useEffect(() => {
-    ReactGA.initialize(TRACKING_ID);
-    // This line is for page view tracking on initial load
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
-  // Track page views on route changes
-  const TrackPageViews = () => {
-    const location = useLocation();
-
-    useEffect(() => {
-      ReactGA.pageview(location.pathname + location.search);
-    }, [location]);
-
-    return null;
-  };
-
   return (
     <div className="App">
       <Router>
         <Routes>
+        <Route path="*" element={<TrackPageViews />} />
+          <Route element={<RouteChangeTracker />} />
           <Route path="/" element={<LoginPage />} />
-          <Route path="*" element={<TrackPageViews />} />
+          <Route
+            path="*"
+            element={
+              <div>
+                <ProtectedRoutes />
+              </div>
+            }
+          />
         </Routes>
-
-        <div style={{ display: 'flex' }}>
-          <div style={{ width: '100%' }}>
-            <Routes>
-              <Route
-                path="/home"
-                element={
-                  <Protected>
-                    <Cards />
-                  </Protected>
-                }
-              />
-              <Route
-                path="/tlprocess"
-                element={
-                  <Protected>
-                    <Process />
-                  </Protected>
-                }
-              />
-              <Route
-                path="/settings"
-                element={
-                  <Protected>
-                    <Settings />
-                  </Protected>
-                }
-              />
-            </Routes>
-          </div>
-        </div>
       </Router>
     </div>
   );
 }
+
+// Protected routes component
+const ProtectedRoutes = () => (
+  <div style={{ display: 'flex' }}>
+    <div style={{ width: '100%' }}>
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            <Protected>
+              <Cards />
+            </Protected>
+          }
+        />
+        <Route
+          path="/tlprocess"
+          element={
+            <Protected>
+              <Process />
+            </Protected>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <Protected>
+              <Settings />
+            </Protected>
+          }
+        />
+      </Routes>
+    </div>
+  </div>
+);
 
 export default App;
